5eab193cce87ff52ee01fd557d5f2ae5e9275eeb
Sergey Mizin <s.mizin@itsalt.ru>
2018-02-07 17:41:47 +0400

    SignupScene final design 65%

--- src/scenes/Signup/index.js
+++ src/scenes/Signup/index.js
@@ -3,12 +3,12 @@ import { Text, View, Picker, ScrollView } from 'react-native';
 import { connect } from 'react-redux';
 import { Actions } from 'react-native-router-flux';
 // import { emailChanged, passwordChanged, loginUser } from '../actions';
-import { FormLabel, FormInput, Button, Icon } from 'react-native-elements';
+import { FormLabel, FormInput, Button, Icon, Divider } from 'react-native-elements';
 
 import Background from '../../components/common/Background';
 import Row from '../../components/common/Row';
 import Logo from '../../components/common/Logo';
-import { signupStyles } from '../../styles';
+import styles from './styles';
 
 const cities = [
   'Тольятти',
@@ -17,6 +17,33 @@ const cities = [
 ];
 
 class SignupForm extends Component {
+  state = {
+    firstName: {
+      active: false,
+      valid: false
+    },
+    lastName: {
+      active: false,
+      valid: false
+    },
+    fatherName: {
+      active: false,
+      valid: false
+    },
+    phone: {
+      active: false,
+      valid: false
+    },
+    password: {
+      active: false,
+      valid: false
+    },
+    passwordRe: {
+      active: false,
+      valid: false
+    }
+  }
+
   onButtonPress() {
 
   }
@@ -25,19 +52,23 @@ class SignupForm extends Component {
   }
 
   render() {
+    const {
+      mainContainerStyle, formLabelStyle, formInputStyle, agreeTextStyle, dividerStyle, boldText,
+      outsideTextStyle
+    } = styles;
     return (
-      <Background>
-        <View style={signupStyles.mainContainerStyle}>
+      <Background type="one">
+        <View style={mainContainerStyle}>
           <ScrollView >
             <Row extraStyles={{ justifyContent: 'space-around', alignItems: 'flex-start', marginTop: 15 }}>
               <View>
-                <FormLabel labelStyle={[signupStyles.formLabelStyle, { marginLeft: 0, marginBottom: 10 }]}>РЕГИСТРИРУЙТЕСЬ ЧЕРЕЗ</FormLabel>
+                <FormLabel labelStyle={[formLabelStyle, { marginLeft: 0, marginBottom: 10 }]}>РЕГИСТРИРУЙТЕСЬ ЧЕРЕЗ</FormLabel>
                 <Row>
                   <Icon
                     name="facebook"
                     type="font-awesome"
                     reverse
-                    color="midnightblue"
+                    color="#415fa8"
                     size={18}
                     containerStyle={{ margin: 0, marginRight: 10 }}
                   />
@@ -45,7 +76,7 @@ class SignupForm extends Component {
                     name="vk"
                     type="font-awesome"
                     reverse
-                    color="deepskyblue"
+                    color="#0077d9"
                     size={18}
                     containerStyle={{ margin: 0 }}
                   />
@@ -53,17 +84,40 @@ class SignupForm extends Component {
               </View>
               <Logo />
             </Row>
-            <FormLabel labelStyle={{ marginTop: 10, marginBottom: 10 }}>ИЛИ ЗАПОЛНИТЕ ФОРМУ</FormLabel>
-            <FormLabel labelStyle={[signupStyles.formLabelStyle]}>ФАМИЛИЯ*</FormLabel>
-            <FormInput inputStyle={[signupStyles.formInputStyle]} onChangeText={() => {}} />
-            <FormLabel labelStyle={[signupStyles.formLabelStyle]}>ИМЯ*</FormLabel>
-            <FormInput inputStyle={[signupStyles.formInputStyle]} onChangeText={() => {}} />
-            <FormLabel labelStyle={[signupStyles.formLabelStyle]}>ОТЧЕСТВО</FormLabel>
-            <FormInput inputStyle={[signupStyles.formInputStyle]} onChangeText={() => {}} />
-            <FormLabel labelStyle={[signupStyles.formLabelStyle]}>ТЕЛЕФОН*</FormLabel>
-            <FormInput inputStyle={[signupStyles.formInputStyle]} onChangeText={() => {}} />
+            <FormLabel labelStyle={[formLabelStyle, { marginTop: 10, marginBottom: 10 }]}>ИЛИ ЗАПОЛНИТЕ ФОРМУ</FormLabel>
+            <FormLabel labelStyle={formLabelStyle}>ФАМИЛИЯ*</FormLabel>
+            <FormInput
+              inputStyle={formInputStyle}
+              onChangeText={() => {}}
+              underlineColorAndroid="transparent"
+            />
+            <Divider style={dividerStyle} />
+            <FormLabel labelStyle={formLabelStyle}>ИМЯ*</FormLabel>
+            <FormInput
+              inputStyle={formInputStyle}
+              onChangeText={() => {}}
+              underlineColorAndroid="transparent"
+            />
+            <Divider style={dividerStyle} />
+            <FormLabel labelStyle={formLabelStyle}>ОТЧЕСТВО</FormLabel>
+            <FormInput
+              inputStyle={formInputStyle}
+              onChangeText={() => {}}
+              underlineColorAndroid="transparent"
+            />
+            <Divider style={dividerStyle} />
+            <Row extraStyles={{ justifyContent: 'space-between' }}>
+              <FormLabel labelStyle={formLabelStyle}>ТЕЛЕФОН*</FormLabel>
+              <FormLabel labelStyle={[formLabelStyle]}>+7 000 000 00 00</FormLabel>
+            </Row>
+            <FormInput
+              inputStyle={formInputStyle}
+              onChangeText={() => {}}
+              underlineColorAndroid="transparent"
+            />
+            <Divider style={dividerStyle} />
             <Row extraStyles={{ marginTop: 7, marginBottom: 7 }}>
-              <FormLabel labelStyle={[signupStyles.formLabelStyle, { color: 'blue' }]}>ГОРОД ПРОЖИВАНИЯ*</FormLabel>
+              <FormLabel labelStyle={[formLabelStyle, { color: '#091b75', height: 20, textAlignVertical: 'bottom' }]}>ГОРОД ПРОЖИВАНИЯ*</FormLabel>
               <Picker
                 style={{ flex: 1, height: 20 }}
                 selectedValue={0}
@@ -72,28 +126,44 @@ class SignupForm extends Component {
                 {this.renderCities(cities)}
               </Picker>
             </Row>
-            <FormLabel labelStyle={[signupStyles.formLabelStyle]}>ПАРОЛЬ*</FormLabel>
-            <FormInput secureTextEntry inputStyle={[signupStyles.formInputStyle]} onChangeText={() => {}} />
+            <Divider style={dividerStyle} />
+            <FormLabel labelStyle={formLabelStyle}>ПАРОЛЬ*</FormLabel>
+            <FormInput
+              secureTextEntry
+              inputStyle={formInputStyle}
+              onChangeText={() => {}}
+              underlineColorAndroid="transparent"
+            />
+            <Divider style={dividerStyle} />
             <Row extraStyles={{ justifyContent: 'space-between' }}>
-              <FormLabel labelStyle={[signupStyles.formLabelStyle, { maxWidth: '100%', marginRight: 0, flex: 1 }]}>ПОВТОРИТЕ ПАРОЛЬ*</FormLabel>
-              <FormLabel labelStyle={[signupStyles.formLabelStyle, { maxWidth: '100%', marginLeft: 0, flex: 1 }]}>СОВПАДАЕТ</FormLabel>
+              <FormLabel labelStyle={[formLabelStyle]}>ПОВТОРИТЕ ПАРОЛЬ*</FormLabel>
+              <FormLabel labelStyle={[formLabelStyle]}>СОВПАДАЕТ</FormLabel>
             </Row>
-            <FormInput secureTextEntry inputStyle={[signupStyles.formInputStyle]} onChangeText={() => {}} />
+            <FormInput
+              secureTextEntry
+              inputStyle={[formInputStyle]}
+              onChangeText={() => {}}
+              underlineColorAndroid="transparent"
+            />
+            <Divider style={dividerStyle} />
           </ScrollView>
         </View>
-        <Text style={signupStyles.agreeTextStyle}>
+        <Text style={agreeTextStyle}>
           Регистрируясь, вы подтверждаете, что принимаете
-          <Text style={{ fontWeight: 'bold' }}>{'\n'}Условия использования{' '}</Text>
+          <Text style={boldText}>{'\n'}Условия использования{' '}</Text>
           и соглашаетесь с
-          <Text style={{ fontWeight: 'bold' }}>{'\n'}Политикой конфиденциальности</Text>
+          <Text style={boldText}>{'\n'}Политикой конфиденциальности</Text>
         </Text>
-        <Row extraStyles={{ maxHeight: 40, justifyContent: 'space-around', alignItems: 'center' }}>
-          <Text style={{ flex: 1, textAlign: 'center' }} onPress={() => Actions.Auth()}>УЖЕ ЕСТЬ АККАУНТ</Text>
+        <Row extraStyles={{
+          maxHeight: 40, width: '100%', justifyContent: 'space-around', alignItems: 'center'
+        }}
+        >
+          <Text style={outsideTextStyle} onPress={() => Actions.Auth()}>УЖЕ ЕСТЬ АККАУНТ</Text>
           <Row extraStyles={{
-            maxHeight: 40, flex: 1, justifyContent: 'center', alignItems: 'center'
+            maxHeight: 40, justifyContent: 'center', alignItems: 'center'
           }}
           >
-            <Text style={{ textAlign: 'center' }}>ОТПРАВИТЬ</Text>
+            <Text style={outsideTextStyle}>ОТПРАВИТЬ</Text>
             <Icon
               name="arrow-right"
               type="font-awesome"
---
+++ src/scenes/Signup/styles.js
@@ -0,0 +1,48 @@
+export default {
+  boldText: {
+    fontWeight: 'bold'
+  },
+  mainContainerStyle: {
+    maxWidth: '80%',
+    height: '70%',
+    backgroundColor: 'white',
+    alignItems: 'flex-start',
+    justifyContent: 'center',
+    alignSelf: 'center'
+  },
+  formInputStyle: {
+    fontSize: 20,
+    maxWidth: '100%',
+    minHeight: 20,
+    paddingTop: 0,
+    paddingBottom: 5,
+    marginTop: 0
+  },
+  formLabelStyle: {
+    marginTop: 0,
+    fontSize: 12,
+    maxWidth: '100%',
+    alignSelf: 'flex-start'
+  },
+  agreeTextStyle: {
+    width: '80%',
+    textAlign: 'center',
+    fontSize: 10,
+    color: 'white',
+    marginTop: 15,
+    marginBottom: 10
+  },
+  dividerStyle: {
+    marginBottom: 10,
+    marginTop: 3,
+    backgroundColor: '#bfbfbf',
+    width: '90%',
+    alignSelf: 'center'
+  },
+  outsideTextStyle: {
+    textAlign: 'center',
+    color: 'white',
+    fontSize: 12,
+    fontWeight: '400'
+  }
+};
